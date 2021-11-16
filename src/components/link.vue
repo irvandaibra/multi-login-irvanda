0374258b0be42830b63c627b04e246d820736ecd
irvandaibra <irvndibhm354@gmail.com>
2021-11-16 11:43:28 +0700

    multi login

---
+++ src/components/link.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="th">
+        <button v-if="userRole === 'siswa'">siswa</button>
+        <button v-if="userRole === 'guru'">guru</button>
+        <button v-if="userRole === 'admin'">admin</button>
+    </div>
+</template>
+<script>
+export default{
+    name: "link",
+    computed: {
+        userRole(){
+            return JSON.parse(sessionStorage.getItem("USER_DATA")).role;
+        },
+    },
+};
+</script>
+<style>
+    .th {
+        text-align: center;
+    }
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1 class="h1">SELAMAT DATANG DI NAVBAR</h1>
+    </div>
+</template>
+<style>
+    .h1 {
+        text-align: center;
+        color: aqua;
+
+    }
+</style>
